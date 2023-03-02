5d9f2900dc56f298ee2a70fa9a9e451efc0060d5
n <npyee@stu.naperville203.org>
2023-03-02 11:16:07 -0600

    final state on this branch before experimentation (still doesn't work)

--- static/ts/indexConnect.ts
+++ static/ts/indexConnect.ts
@@ -1,7 +1,7 @@
 /// <reference path="./socket-io-client.d.ts"/>
 
 var indexSocket = window.io('/'); // This line and the line above is how you get ts types to work on clientside... cursed
-// do NOT import anything or ejs will get angry
+// !!! do NOT import/export anything or ejs will get angry
 
 var pins: number[] = [];
 updatePins();
@@ -10,7 +10,7 @@ indexSocket.on("update", (data: any) => {
     let inPins: number[] = [];
     let inPinsBus: Bus[] = [];
     updatePins();
-    for (let i = 0; i < data.buses.length(); i++) { // filter numbers into in the pin list or not
+    for (let i = 0; i < data.buses.length(); i++) { // filter numbers based on whether or not they are in the pinlist
         if (pins.includes(data.buses[i].number)) {
             inPins.push(data.buses[i].number);
             inPinsBus.push(data.buses[i]);
@@ -20,11 +20,11 @@ indexSocket.on("update", (data: any) => {
 
     const htmlPins = ejs.render(document.getElementById("renderPins")!.getAttribute("render")!, {data: pinData});
     const htmlAll = ejs.render(document.getElementById("renderAll")!.getAttribute("render")!, {data: data});
-    // @ts-ignore
-    document.getElementById("pinBus").innerHTML = htmlPins;
-    // @ts-ignore
-    document.getElementById("allBus").innerHTML = htmlAll;
+
+    document.getElementById("pinBus")!.innerHTML = htmlPins;
+    document.getElementById("allBus")!.innerHTML = htmlAll;
     updatePins();
+    updatePinTable();
 });
 
 function updatePins() { // call (very) (extremely) often cause this resets every time the user, the server, or the admin does anything
@@ -39,16 +39,23 @@ function updatePins() { // call (very) (extremely) often cause this resets every
     }
 }
 
+function updatePinTable() {
+    let allTable:HTMLTableElement = <HTMLTableElement> document.getElementById("all-bus-table");
+    let rows = allTable.rows;
+    let row:HTMLTableRowElement = <HTMLTableRowElement> rows[0];
+    updatePins();
+    for (let j = 1; j < rows.length; j++) {
+        row = rows[j];
+        let num:number = parseInt(row.firstElementChild!.innerHTML);
+        if (pins.includes(num)) {
+            allTable.deleteRow(j);
+        }
+    }
+}
+
 function pinBus(button: HTMLInputElement) {
     updatePins();
     const busRow = button.parentElement!.parentElement; // this is the overarching <tr> element of the bus row
-    const busTableBody = busRow!.parentElement; // <tbody>
-    var workingRow = busTableBody!.firstElementChild;
-    var busList:Bus[] = [];
-    while (workingRow?.nextElementSibling) {
-        
-        workingRow = workingRow?.nextElementSibling;
-    }
     const busNumber = busRow!.firstElementChild!.innerHTML; // this is the stringification of the number of the bus
     
     const num = parseInt(busNumber); // this is the number of the bus
@@ -73,17 +80,7 @@ function pinBus(button: HTMLInputElement) {
             }
         }
     }
-
-    // ok this is the part where we try to update the webpage
-    updatePins();
-    /*
-    for (let i = 0; i < pins.length; i++) {
-        console.log(pins[i].toString());
-    } */
-    let inPinsBus: Bus[] = [];
-    for (let i = 0; i < pins.length; i++) {
-        
-    }
+    updatePinTable();
 }
 
 
