5e6d3e4bd0a7f8c4e366b702bece564a5f9dbc88
WSBarnes <wsbarnes@stu.naperville203.org>
2023-03-02 11:40:42 -0600

    REGISTERED SERVICE WORKER

--- static/ts/indexConnect.ts
+++ static/ts/indexConnect.ts
@@ -10,3 +10,12 @@ indexSocket.on("update", (data) => {
     const html = ejs.render(document.getElementById("getRender")!.getAttribute("render")!, {data: data});
     document.getElementById("content")!.innerHTML = html;
 });
+
+if('serviceWorker' in navigator){
+    //checks if serviceWorkers work within the search engine
+    navigator.serviceWorker.register('/sw.js')
+        .then((reg) => console.log('service worker registered!', reg))
+        .catch((err) => console.log('service worker not registered; error :( ', err))
+    //registers serviceWorker "sw.js" If register is successful. ".then" happens. ".catch" catches the error if it doesn't happen for whatever reason.
+    //No matter what, this will lead to some value. This is called a promise, as unlike a function, it'll always result in some output.
+}
