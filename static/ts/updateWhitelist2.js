a4e942fd973d3ace87bce59df20adbe37b87a891
Kslovisa <89259663+Kslovisa@users.noreply.github.com>
2023-04-05 12:02:59 -0500

    worked on updating whitelist page

--- static/ts/updateWhitelist2.js
+++ static/ts/updateWhitelist2.js
@@ -12,26 +12,12 @@ var admins;
 fetch("/whitelistFile").then((data) => data.json()).then((data) => admins = data);
 var newAdminEmptyRow;
 fetch("/adminEmptyRow").then((res) => res.text()).then((data) => newAdminEmptyRow = data);
-function addAdmin_admins2(newAddress) {
-    if (newAddress.includes('@') && newAddress.includes('naperville203.org') && (newAddress.indexOf('@') < newAddress.indexOf('naperville203.org')) && (!admins.includes(newAddress))) {
-        console.log(newAddress);
-        console.log(newAdminEmptyRow);
-        const row = document.getElementsByClassName("buslist-table")[0].insertRow(1);
-        const html = ejs.render(newAdminEmptyRow, { newAddress: newAddress });
-        console.log(html);
-        row.innerHTML = html;
-        admins.splice(0, 0, newAddress);
-        fetch("/whitelistFile", {
-            method: 'POST',
-            headers: {
-                accept: 'application.json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                admins: admins
-            })
-        });
-    }
+function addAdmin_admins2() {
+    const row = document.getElementsByClassName("buslist-table")[0].insertRow(2);
+    const html = ejs.render(newAdminEmptyRow);
+    row.innerHTML = html;
+    let input = row.children[0].children[0];
+    input.focus();
 }
 function save2(reset) {
     return __awaiter(this, void 0, void 0, function* () {
--- static/ts/updateWhitelist2.ts
+++ static/ts/updateWhitelist2.ts
@@ -3,28 +3,13 @@ fetch("/whitelistFile").then((data)=>data.json()).then((data) => admins = data);
 
 var newAdminEmptyRow: string;
 fetch("/adminEmptyRow").then((res) => res.text()).then((data) => newAdminEmptyRow = data);
-
-function addAdmin_admins2(newAddress: string) {
-    if(newAddress.includes('@') && newAddress.includes('naperville203.org') && (newAddress.indexOf('@') < newAddress.indexOf('naperville203.org')) && (!admins.includes(newAddress))){
-        console.log(newAddress)
-        console.log(newAdminEmptyRow)
-        const row = (<HTMLTableElement> document.getElementsByClassName("buslist-table")[0]).insertRow(1);
-        const html = ejs.render(newAdminEmptyRow,{newAddress: newAddress});
-        console.log(html);
-        row.innerHTML = html;
-        admins.splice(0, 0, newAddress);
-        fetch("/whitelistFile", {
-            method: 'POST',
-            headers: {
-            accept: 'application.json',
-            'Content-Type': 'application/json'
-            },
-            body: 
-            JSON.stringify({
-               admins: admins
-            })
-        })
-    }
+ 
+function addAdmin_admins2() {
+    const row = (<HTMLTableElement> document.getElementsByClassName("buslist-table")[0]).insertRow(2);
+    const html = ejs.render(newAdminEmptyRow);
+    row.innerHTML = html;
+    let input = row.children[0]!.children[0] as HTMLInputElement;
+    input.focus();
     
 }
 
