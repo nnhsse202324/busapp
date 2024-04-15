114765d34cfb63c57447ee321bcd2eef4a2f1008
DevamDas1 <ddas@stu.naperville203.org>
2024-04-15 09:34:20 -0500

    Some moving over done

---
+++ server/model/wave.ts
@@ -0,0 +1,20 @@
+/**
+ * schema for a journal entry
+ */
+export {};
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    locked: {
+        type: Boolean,
+        required: true,
+    },
+    leavingAt: {
+        type: Date,
+        required: true,
+    }
+});
+
+const Wave = mongoose.model("Wave", schema);
+
+module.exports = Wave;
